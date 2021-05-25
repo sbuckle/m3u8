e54a29f13dd72219779af59d4a2da4fbb6bfe9da
Simon Buckle <simon.buckle@gmail.com>
2021-05-25 15:56:45 +0100

    Prints absolute URL of each variant stream

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"path/filepath"
 
@@ -14,11 +15,23 @@ func main() {
 		fmt.Printf("Usage: %s <url>\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
+	u, err := url.Parse(os.Args[1]) // Check
+	if err != nil {
+		log.Fatalf("Invalid argument: %v\n", err)
+	}
 	p, err := hls.ParsePlaylist(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, s := range p.Segments {
 		fmt.Printf("%s\n", s.Url)
+	}
+
+	for _, v := range p.Variants {
+		rel, err := u.Parse(v.Url)
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%s\n", rel.String())
 	}
 }
