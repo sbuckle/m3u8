9a5e06de30f28b7e7766e50fa00c92a19f633143
Simon Buckle <simon.buckle@gmail.com>
2021-05-31 09:07:16 +0100

    Added test for parsing an attribute list

--- parse_test.go
+++ parse_test.go
@@ -1,6 +1,7 @@
 package m3u8
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -17,6 +18,27 @@ func TestParseByteRange(t *testing.T) {
 	for _, test := range tests {
 		if l, o := parseByteRange(test.input); l != test.length || o != test.offset {
 			t.Errorf("parseByteRange(%q) = %d %d", test.input, l, o)
+		}
+	}
+}
+
+func TestParseAttributeList(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  map[string]string
+	}{
+		{
+			`BANDWIDTH=3389529,CODECS="mp4a.40.2,avc1.4d401e"`,
+			map[string]string{"BANDWIDTH": "3389529", "CODECS": `mp4a.40.2,avc1.4d401e`},
+		},
+		{
+			`URI="https://priv.example.com/key.php?r=52"`,
+			map[string]string{"URI": `https://priv.example.com/key.php?r=52`},
+		},
+	}
+	for _, test := range tests {
+		if got := parseAttributeList(test.input); !reflect.DeepEqual(test.want, got) {
+			t.Errorf("parseAttributeList(%q) = %q", test.input, got)
 		}
 	}
 }
